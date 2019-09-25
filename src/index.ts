d0a6e7bb9423f3342a6fe14a6570a772e811f5fa
Theak <theakman2@hotmail.com>
2019-09-25 05:12:33 +0100

    Remove HtmlPrinter: was too similar to TextPrinter

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,11 @@
 import {FormatStringParser} from './lib/FormatStringParser';
 import {BinaryContentFormatter} from './lib/BinaryContentFormatter';
 import {Validator} from './lib/Validator';
-import {HtmlPrinter} from './lib/printers/HtmlPrinter';
 import {TextPrinter} from './lib/printers/TextPrinter';
 
 export {
 	FormatStringParser,
 	BinaryContentFormatter,
 	Validator,
-	HtmlPrinter,
 	TextPrinter,
 };
